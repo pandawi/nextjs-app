85055bac9275b1e4f7aaeb4e909e5ec3cf26868c
Saar Kuriel <skuriel@cisco.com>
2025-03-26 12:24:19 +0200

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Enable standalone build
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
